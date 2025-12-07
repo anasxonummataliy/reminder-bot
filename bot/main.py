d4df50b0d7d99614d133b74f04b9f19257203dd3
Anasxon Ummataliyev <aummataliy@gmail.com>
2025-12-07 22:21:16 +0500

    fix

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,25 @@
+import asyncio
+import logging
 import os
+import sys
+from aiogram.types import Message
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+
+dp = Dispatcher()
+bot = Bot(token=os.getenv("TOKEN"))
+
+dp.message()
+async def start_handler(message: Message):
+    await message.answer("Salom")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
+
---
+++ bot/router/reminder.py
@@ -0,0 +1,1 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
